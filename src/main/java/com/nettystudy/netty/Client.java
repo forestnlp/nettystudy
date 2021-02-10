f8a57064f31d3736a5d8079b548fd7e205289f2e
forestnlp <19085498@qq.com>
2021-02-10 11:30:17 +0800

    实现了netty的client和server

--- src/main/java/com/nettystudy/netty/Client.java
+++ src/main/java/com/nettystudy/netty/Client.java
@@ -1,8 +1,73 @@
 package com.nettystudy.netty;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 public class Client {
+    public static void main(String[] args) {
+        EventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        try {
+            ChannelFuture future = bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new CilentChannelInitializer());
+                        }
+                    })
+                    .connect("localhost", 8888);
 
+            future.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if(!future.isSuccess()) {
+                        System.out.println("not connected");
+                    }
+                    else {
+                        System.out.println("conntected");
+                    }
+                }
+            });
 
+            future.sync();
+            System.out.println("........");
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            group.shutdownGracefully();
+        }
+    }
+}
 
+class CilentChannelInitializer extends ChannelInitializer<SocketChannel>{
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        System.out.println("CilentChannelInitializer:"+ch);
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new ClientHandler());
+    }
+}
+
+class ClientHandler extends ChannelInboundHandlerAdapter{
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        //第一次连上的时候，就写出一个字符串
+        ByteBuf buf = Unpooled.copiedBuffer("hello,i am netty client".getBytes());
+        ctx.writeAndFlush(buf);
+    }
 }
--- src/main/java/com/nettystudy/netty/Server.java
+++ src/main/java/com/nettystudy/netty/Server.java
@@ -1,8 +1,63 @@
 package com.nettystudy.netty;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+
 public class Server {
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(2);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup(32);
 
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        try {
+            ChannelFuture future = serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            System.out.println(ch);
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new ServerChildHandler());
+                        }
+                    }).bind(8888)
+                    .sync();
+            System.out.println("netty server started!");
+            future.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    System.out.println("connected");
+                }
+            });
+            //channel的future等待关闭命令 通过sync命令，卡在此处，使主程序继续执行
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }  finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
 
-
-
+class ServerChildHandler extends ChannelInboundHandlerAdapter{
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = null;
+        try {
+            buf = (ByteBuf)msg;
+            byte[] bytes = new byte[buf.readableBytes()];
+            buf.getBytes(buf.readerIndex(),bytes);
+            System.out.println(new String(bytes));
+            System.out.println(buf.refCnt());
+        } finally {
+            if(buf!=null)
+                ReferenceCountUtil.release(buf);
+            System.out.println(buf.refCnt());
+        }
+    }
 }
